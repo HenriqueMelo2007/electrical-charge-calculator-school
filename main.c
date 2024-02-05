f5efd01cb48314d38e4c6b3860aac11dc99bb107
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-02-05 18:08:08 -0300

    feat: adiciona variáveis que representam a carga elementar e define argumentos para funções

--- main.c
+++ main.c
@@ -2,12 +2,36 @@
 #include <locale.h>
 #include <stdlib.h>
 
+void calcQ (elementaryChargeCoefficient, elementaryChargeExponent);
+void calcN (elementaryChargeCoefficient, elementaryChargeExponent);
+
 int main(void)
 {
   setlocale(LC_ALL, "Portuguese_Brazil");
 
-  printf("CALCULADORA CARGA ELÉTRICA\n\nESCOLHA O QUE DESEJA CALCULAR:\n\n1 - Q ( CARGA ELÉTRICA )\n2 - N ( QUANTIDADE DE ELÉTRONS )");
+  printf(" CALCULADORA CARGA EL�TRICA\n\n ESCOLHA O QUE DESEJA CALCULAR:\n\n 1 - Q ( CARGA EL�TRICA )\n 2 - N ( QUANTIDADE DE EL�TRONS )\n\n");
+
+  int operation;
+
+  float elementaryChargeCoefficient = 1.6;
+  int elementaryChargeExponent = -19;
+
+
+  do {
+    printf(" ");
+    scanf("%i", &operation);
+  } while (operation != 1 && operation != 2);
+
+  if ( operation == 1 ) {
+    calcQ(elementaryChargeCoefficient, elementaryChargeExponent);
+  } else {
+    calcN(elementaryChargeCoefficient, elementaryChargeExponent);
+  }
 
 
   return 0;
 }
+
+void calcQ (elementaryChargeCoefficient, elementaryChargeExponent) {}
+
+void calcN (elementaryChargeCoefficient, elementaryChargeExponent) {}
